01048625946879c0fa2cb2896037cbd3ab19bb3e
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-11-28 09:58:42 +0700

    UPDATE : method index HasilAngketController telah selesai mengambil data dan mengirimkannya ke view

--- app/Http/Controllers/HasilAngketController.php
+++ app/Http/Controllers/HasilAngketController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AngketTf;
 use Illuminate\Http\Request;
 
 class HasilAngketController extends Controller
 {
-    function index()
+    function index(Request $req)
     {
-        return view('hasil-angket.index');
+        // Ambil semua smt yg ada di angkettf untuk dimasukkan ke select
+        $semuaSmt = AngketTf::select('smt')
+            ->distinct()
+            ->orderBy('smt', 'desc')
+            ->get();
+
+        // Kalo gk ada query param "smt", maka langsung return view
+        if (!$req->smt) return view('hasil-angket.index', compact('semuaSmt'));
+
+        // Ambil semua dosen pada angketTf yang mengajar pada semester tertentu
+        // Setelah diambil, sorting datanya berdasarkan nama dosen
+        $semuaDosen = AngketTf::query()
+            ->dosenMengajarDiSmt($req->smt)
+            ->with('karyawan:nik,nama')
+            ->get()
+            ->sortBy('karyawan.nama');
+
+        return view('hasil-angket.index', compact('semuaSmt', 'semuaDosen'));
     }
 
     function detail($smt, $nik)
--- resources/views/hasil-angket/index.blade.php
+++ resources/views/hasil-angket/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('html_title', 'Hasil Angket')
 
+@push('css')
+    <link href="{{ asset('assets/libs/select2/select2.min.css') }}" rel="stylesheet" />
+
+    <link href="{{ asset('assets/libs/select2/theme/select2-bootstrap-5-theme.min.css') }}" rel="stylesheet" />
+@endpush
+
 @section('content')
     <div class="page-wrapper">
 
@@ -34,12 +40,14 @@
                                     Silahkan pilih semester untuk melihat hasil angket
                                 </p>
                                 <div class="d-flex">
-                                    <select class="form-select me-2">
-                                        <option>231</option>
-                                        <option>222</option>
-                                        <option>221</option>
+                                    <select class="form-select" id="pilihSmt">
+                                        @foreach ($semuaSmt as $smt)
+                                            <option {{ request('smt') == $smt->smt ? 'selected' : '' }}>
+                                                {{ $smt->smt }}
+                                            </option>
+                                        @endforeach
                                     </select>
-                                    <button type="button" class="btn btn-primary">
+                                    <button type="button" class="btn btn-primary ms-2" onclick="goToSemester()">
                                         Tampilkan
                                     </button>
                                 </div>
@@ -47,6 +55,13 @@
                         </div>
                     </div>
                 </div>
+
+                @php
+                    // Kalo query param "smt" null, maka skip looping dosen
+                    if (!request('smt')) {
+                        goto SKIP_LOOP_DOSEN;
+                    }
+                @endphp
 
 
                 <div class="row">
@@ -69,54 +84,30 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td class="text-center">1</td>
-                                                <td class="text-center">980249</td>
-                                                <td class="">Erwin Sutomo</td>
-                                                <td>
-                                                    <a href="" class="btn btn-outline-primary w-100 btn-sm">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                                            stroke-linecap="round" stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                                            <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                                                        </svg>
-                                                        Tampilkan
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="text-center">2</td>
-                                                <td class="text-center">209131</td>
-                                                <td class="">Desita Rizky Amelia Kusumaningtyas</td>
-                                                <td>
-                                                    <a href="" class="btn btn-outline-primary w-100 btn-sm">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                                            stroke-linecap="round" stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                                            <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                                                        </svg>
-                                                        Tampilkan
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="text-center">3</td>
-                                                <td class="text-center">180880</td>
-                                                <td class="">I Gusti Ngurah Alit Widana Putra</td>
-                                                <td>
-                                                    <a href="" class="btn btn-outline-primary w-100 btn-sm">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                                            stroke-linecap="round" stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                                            <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                                                        </svg>
-                                                        Tampilkan
-                                                    </a>
-                                                </td>
-                                            </tr>
+                                            @foreach ($semuaDosen as $dosen)
+                                                <tr>
+                                                    <td class="text-center">
+                                                        {{ $loop->iteration }}.
+                                                    </td>
+                                                    <td class="text-center">
+                                                        {{ $dosen->nik }}
+                                                    </td>
+                                                    <td class="">
+                                                        {{ $dosen->karyawan->nama ?? null }}
+                                                    </td>
+                                                    <td>
+                                                        <a href="" class="btn btn-outline-primary w-100 btn-sm">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                                                stroke-linecap="round" stroke-linejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                                                <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                                            </svg>
+                                                            Tampilkan
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
@@ -124,9 +115,43 @@
                         </div>
                     </div>
                 </div>
+
+                @php
+                    SKIP_LOOP_DOSEN:
+                @endphp
+
             </div>
         </div>
 
     </div>
 
 @endsection
+
+
+@push('js')
+    <script src="{{ asset('assets/libs/select2/select2.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#pilihSmt').select2({
+                theme: 'bootstrap-5'
+            });
+        });
+    </script>
+
+    <script>
+        function goToSemester() {
+            // Ambil nilai semester
+            const selectedSemester = document.getElementById('pilihSmt').value;
+
+            // Membuat objek URL dari URL saat ini
+            const currentUrl = new URL(window.location.href);
+
+            // Menambahkan parameter query "smt" dengan nilai semester
+            currentUrl.searchParams.set('smt', selectedSemester);
+
+            // Mengarahkan ke URL yang baru dibuat
+            window.location.href = currentUrl.href;
+        }
+    </script>
+@endpush
