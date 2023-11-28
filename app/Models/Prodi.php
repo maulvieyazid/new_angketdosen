ebab20c33b4414049eaab19a3518f4a7fe55a5d4
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-11-28 09:47:04 +0700

    tambah scope dan relationship pada model Prodi

--- app/Models/Prodi.php
+++ app/Models/Prodi.php
@@ -14,4 +14,18 @@ class Prodi extends Model
     public $timestamps = false;
 
     public $incrementing = false;
+
+
+
+    // RELATIONSHIP
+    public function fakultas()
+    {
+        return $this->belongsTo(Fakultas::class, 'id_fakultas', 'id');
+    }
+
+    // SCOPE
+    public function scopeAktif($query)
+    {
+        return $query->where('sts_aktif', 'Y');
+    }
 }
