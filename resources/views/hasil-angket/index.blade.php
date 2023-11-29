0e7f50a711910468fc9f7389769a512c996aac9e
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-11-29 15:43:29 +0700

    tambah tombol download excel di view index

--- resources/views/hasil-angket/index.blade.php
+++ resources/views/hasil-angket/index.blade.php
@@ -75,7 +75,22 @@
                     <div class="col">
                         <div class="card">
                             <div class="card-body">
-                                <h3 class="card-title">Dosen mengajar di semester 231</h3>
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h3 class="card-title">Dosen mengajar di semester 231</h3>
+                                    <a href="{{ route('download-excel.hasil-angket', ['smt' => request('smt')]) }}" class="btn btn-success" target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-type-xls" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                            stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                            <path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" />
+                                            <path d="M4 15l4 6" />
+                                            <path d="M4 21l4 -6" />
+                                            <path d="M17 20.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75" />
+                                            <path d="M11 15v6h3" />
+                                        </svg>
+                                        Download Excel Hasil Angket
+                                    </a>
+                                </div>
                                 <div class="table-responsive">
                                     <table class="table table-vcenter card-table table-striped table-hover table-group-divider table-bordered" id="tabelDosen">
                                         <thead>
