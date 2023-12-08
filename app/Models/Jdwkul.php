1eae61cf398a2e649ede0bc6dfba7baabb487655
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-12-08 09:32:34 +0700

    tambah model Krs, Jdwkul, Jdwprk

---
+++ app/Models/Jdwkul.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jdwkul extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+
+    public $timestamps = false;
+
+    public $incrementing = false;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+}
