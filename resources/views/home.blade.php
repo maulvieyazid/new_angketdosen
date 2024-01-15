135fd6681c84d67e6eca8b5a1ea8155295236c6c
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2024-01-15 13:50:58 +0700

    MINOR : ubah tipe chart rata-rata nilai angket anda per semester dari bar menjadi line

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -201,14 +201,17 @@
         const chartAvgNilaiPerSmt = new ApexCharts(
             document.querySelector('#chartAvgNilaiPerSmt'), {
                 chart: {
-                    type: 'bar',
+                    type: 'line',
                     height: '300px',
                     toolbar: {
                         show: false,
                     },
                     events: {
-                        dataPointSelection: async function(event, chartContext, config) {
-                            const dataNilai = config.w.globals.initialSeries[config.seriesIndex].data[config.dataPointIndex];
+                        click: async function(event, chartContext, config) {
+                            // Kalo point yang diklik index nya kurang dari 0 maka return
+                            if (config.dataPointIndex < 0) return;
+
+                            const dataNilai = config.globals.initialSeries[config.seriesIndex].data[config.dataPointIndex];
 
                             let url = "{{ route('detail.hasil-angket', ['data' => ':data']) }}";
                             url = url.replace(':data', dataNilai.encData);
@@ -217,10 +220,14 @@
                         },
                     },
                 },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                    }
+                stroke: {
+                    curve: 'straight',
+                },
+                markers: {
+                    size: 1,
+                },
+                dataLabels: {
+                    enabled: true,
                 },
                 theme: {
                     palette: 'palette1',
@@ -228,11 +235,16 @@
                 legend: {
                     show: false,
                 },
+                grid: {
+                    padding: {
+                        left: 30,
+                    },
+                },
                 tooltip: {
                     enabled: true,
                     x: {
                         formatter: function(value, opts) {
-                            const smt = opts.w.globals.labels[opts.dataPointIndex];
+                            const smt = opts.w.globals.categoryLabels[opts.dataPointIndex];
                             return `Semester ${smt}`;
                         },
                     },
